9b29bb7a58e1349843ed26983fcd9a064f4ef167
Abdelrahman.Yassien11 <Abdelrahman.Yassien11@gmail.com>
2024-10-05 23:45:24 +0300

    more designs

--- clock_divider.v
+++ clock_divider.v
@@ -1,19 +1,23 @@
+`define dff
 module clock_divider#(parameter DIV = 2)
 	(	
 		input clk, resetn,
-		output reg clk_div
+		output clk_div
 		);
 
+`ifdef latch_counter
+
 	integer counter;
+	reg clk_div_reg;
 
 	always@(clk or negedge resetn) begin
 		if(!resetn)begin
 			counter <= 0;
-			clk_div <= 0;
+			clk_div_reg <= 0;
 		end
 		else begin
 			if(counter == DIV-1) begin
-				clk_div <= ~clk_div;
+				clk_div_reg <= ~clk_div_reg;
 				counter <= 0;
 			end
 			else begin
@@ -21,5 +25,44 @@ module clock_divider#(parameter DIV = 2)
 			end
 		end
 	end
+	assign  clk_div = clk_div_reg;
 
+`elsif dff_counter
+
+	integer counter;
+	reg clk_div_reg;
+
+	always@(posedge clk or negedge resetn) begin
+		if(!resetn)begin
+			counter <= 0;
+			clk_div_reg <= 0;
+		end
+		else begin
+			if(counter < DIV-1) begin
+				counter <= counter + 1;
+			end
+			else begin
+				clk_div_reg <= ~clk_div_reg;
+				counter <= 0;
+			end
+		end
+	end
+	assign  clk_div = clk_div_reg;	
+
+`elsif dff
+	
+	reg [DIV-1:0] clk_div_reg;
+
+	always@(posedge clk or negedge resetn) begin
+		if(!resetn)begin
+			clk_div_reg <= 0;
+		end
+		else begin
+			clk_div_reg <= clk_div_reg[{clk_div_reg[DIV-2:0],~clk_div}];
+		end
+	end
+
+	assign clk_div = clk_div_reg[DIV-1];
+
+`endif
 endmodule
